e0713101106dba39d04dc2647ad97b25180b89db
TuF3i <13759301652@163.com>
2026-09-26 21:56:32 +0800

    ✨ Fix Tea

--- target/zenbook/home/tools.nix
+++ target/zenbook/home/tools.nix
@@ -10,12 +10,14 @@
       push.autoSetupRemote = true;
 
       # 声明式 credential helper:git 全局配置是 HM 只读文件,
-      # gh/tea 无法运行时写入,helper 直接在此声明,工具只管认证
+      # gh/tea 无法运行时写入,helper 直接在此声明,工具只管认证。
+      # 注意多词 helper 必须 ! 开头,否则 git 会去执行不存在的
+      # git-credential-<首词>(tea 那三条曾因此报 credential-tea 错误)
       credential = {
         "https://github.com".helper = "!gh auth git-credential";
-        "https://git.lan.tuf3i.cc".helper = "tea login helper";
-        "https://git.tuf3i.cc".helper = "tea login helper";
-        "https://git.redrock.team".helper = "tea login helper";
+        "https://git.lan.tuf3i.cc".helper = "!tea login helper";
+        "https://git.tuf3i.cc".helper = "!tea login helper";
+        "https://git.redrock.team".helper = "!tea login helper";
       };
     };
 
